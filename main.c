ac0bf750fe3b30cdab9ecbd968f986abb12062f6
Mohamed adel <mohamed.sayed002@eng-st.cu.edu.eg>
2021-07-10 15:33:56 +0200

    student 1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+ 
+struct student
+    {
+    char name[50];
+	int id;
+	int day, month, year;
+	int lyscore;
+    };
+struct student a;
+
+
+int main()
+{
+ printf("welcome.let's start the program")
+        
+    return 0;
+}
